ff23621dc687f709fcfced498d4ab7738824736f
Ron Hjorth <ron.hjorth@allata.com>
2023-07-06 07:33:57 -0600

    Initial commit

---
+++ lib/models/recipe.api.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:recipe_list/models/recipe.dart';
+
+class RecipeApi {
+  static Future<List<Recipe>> getRecipe() async {
+    var uri = Uri.https('yummly2.p.rapidapi.com', '/feeds/list',
+        {"limit": "18", "start": "0", "tag": "list.recipe.popular"});
+
+    final response = await http.get(uri, headers: {
+      "x-rapidapi-key": "e1e4dc5b56msh081ffc281278e1bp14ca07jsn7c8e2fb33f8e",
+      "x-rapidapi-host": "yummly2.p.rapidapi.com",
+      "useQueryString": "true"
+    });
+
+    Map data = jsonDecode(response.body);
+    List temp = [];
+
+    for (var i in data['feed']) {
+      temp.add(i['content']['details']);
+    }
+
+    return Recipe.recipesFromSnapshot(temp);
+  }
+}
